184ff78f760e2bf99afcd93c37700c4c9e30f297
sahilstan <me@sahilsharma.co>
2019-05-08 04:34:42 +0400

    "project1 updated"

--- project1.py
+++ project1.py
@@ -5,10 +5,9 @@ C - 79-70%
 D - 69-60% 
 F - 59-0% 
 '''
-try:
-	pointsPossible = 67
-	score = int(input("Score? : "))
-	name = input("What is your name? : ")
+
+def cal():
+	pointsPossible = 100
 	percent = ( score / pointsPossible ) * 100
 	grade = ""
 
@@ -24,5 +23,13 @@ try:
 		grade = "F"
 
 	print ("{} score {} % and grade is {}".format(name, round(percent, 2), grade))
-except Exception:
-	print ("Please put valid score!")
+	
+while True:
+	try:
+		name = input("What is your name? : ")	
+		score = int(input("Score? : "))
+		cal()
+		break
+
+	except Exception:
+		print ("Please put valid score!")
